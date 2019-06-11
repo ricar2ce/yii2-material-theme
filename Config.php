630cdffae460797e279cf0a6fcb16027c53c14c5
Ricardo Comendador Escalona <ricar2ce@gmail.com>
2019-06-11 09:39:22 -0400

    add Config Class

---
+++ Config.php
@@ -0,0 +1,40 @@
+<?php
+namespace rce\material;
+
+use Yii;
+
+class Config
+{
+    public static function sidebarColor()
+    {
+        /** @var Asset $bundle */
+        $bundle = Yii::$app->assetManager->getBundle('rce\material\Assets');
+
+        return $bundle->sidebarColor;
+    }
+
+
+    public static function sidebarBackgroundColor()
+    {
+        /** @var Asset */
+        $bundle = Yii::$app->assetManager->getBundle('rce\material\Assets');
+
+        return $bundle->sidebarBackgroundColor;
+    }
+
+    public static function sidebarBackgroundImage()
+    {
+        /** @var Asset */
+        $bundle = Yii::$app->assetManager->getBundle('rce\material\Assets');
+
+        return $bundle->sidebarBackgroundImage;
+    }
+
+    public static function siteTitle()
+    {
+        /** @var Asset */
+        $bundle = Yii::$app->assetManager->getBundle('rce\material\Assets');
+
+        return $bundle->siteTitle;
+    }
+}
