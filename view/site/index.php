384038bd931cf3b1c604c1df25670be3b7e4ca05
Ricardo Comendador Escalona <ricar2ce@gmail.com>
2019-06-15 04:43:04 -0400

    new index design

--- view/site/index.php
+++ view/site/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use rce\material\widgets\Card;
 /* @var $this yii\web\View */
 
 $this->title = 'My Yii Application';
@@ -17,107 +18,73 @@ $this->title = 'My Yii Application';
     <div class="body-content">
 
         <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <?php Card::begin([
+                    'header'=>'header-icon',
+                    'type'=>'card-stats',
+                    'icon'=>'<i class="material-icons">content_copy</i>',
+                    'color'=>'warning',
+                    'title'=>'49/50<small>GB</small>',
+                    'subtitle'=>'Used Space',
+                    'footer'=>'<div class="stats">
+                            <i class="material-icons text-danger">warning</i>
+                            <a href="#">Get More Space...</a>
+                          </div>',
+                ]); Card::end(); ?>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <?php Card::begin([
+                    'header'=>'header-icon',
+                    'type'=>'card-stats',
+                    'icon'=>'<i class="material-icons">store</i>',
+                    'color'=>'success',
+                    'title'=>'$34,245',
+                    'subtitle'=>'Revenue',
+                    'footer'=>'<div class="stats">
+                            <i class="material-icons">date_range</i> Last 24 Hours
+                          </div>',
+                ]); Card::end(); ?>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <?php Card::begin([
+                    'header'=>'header-icon',
+                    'type'=>'card-stats',
+                    'icon'=>'<i class="material-icons">info_outline</i>',
+                    'color'=>'danger',
+                    'title'=>'75',
+                    'subtitle'=>'Fixed Issues',
+                    'footer'=>'<div class="stats">
+                            <i class="material-icons">local_offer</i> Tracked from Github
+                          </div>',
+                ]); Card::end(); ?>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <?php Card::begin([
+                    'type'=>'card-stats',
+                    'header'=>'header-icon',
+                    'icon'=>'<i class="fa fa-twitter"></i>',
+                    'color'=>'info',
+                    'title'=>'+245',
+                    'subtitle'=>'Followers',
+                    'footer'=>'<div class="stats">
+                            <i class="material-icons">update</i> Just Updated
+                          </div>',
+                ]); Card::end(); ?>
+            </div>
+        </div>
+
+        <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
             </div>
         </div>
 
     </div>
 
-      <div class="row">
-        <div class="col-lg-3 col-md-6 col-sm-6">
-          <div class="card card-stats">
-            <div class="card-header card-header-warning card-header-icon">
-              <div class="card-icon">
-                <i class="material-icons">content_copy</i>
-              </div>
-              <p class="card-category">Used Space</p>
-              <h3 class="card-title">49/50
-                <small>GB</small>
-              </h3>
-            </div>
-            <div class="card-footer">
-              <div class="stats">
-                <i class="material-icons text-danger">warning</i>
-                <a href="#pablo">Get More Space...</a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6 col-sm-6">
-          <div class="card card-stats">
-            <div class="card-header card-header-success card-header-icon">
-              <div class="card-icon">
-                <i class="material-icons">store</i>
-              </div>
-              <p class="card-category">Revenue</p>
-              <h3 class="card-title">$34,245</h3>
-            </div>
-            <div class="card-footer">
-              <div class="stats">
-                <i class="material-icons">date_range</i> Last 24 Hours
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6 col-sm-6">
-          <div class="card card-stats">
-            <div class="card-header card-header-danger card-header-icon">
-              <div class="card-icon">
-                <i class="material-icons">info_outline</i>
-              </div>
-              <p class="card-category">Fixed Issues</p>
-              <h3 class="card-title">75</h3>
-            </div>
-            <div class="card-footer">
-              <div class="stats">
-                <i class="material-icons">local_offer</i> Tracked from Github
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6 col-sm-6">
-          <div class="card card-stats">
-            <div class="card-header card-header-info card-header-icon">
-              <div class="card-icon">
-                <i class="fa fa-twitter"></i>
-              </div>
-              <p class="card-category">Followers</p>
-              <h3 class="card-title">+245</h3>
-            </div>
-            <div class="card-footer">
-              <div class="stats">
-                <i class="material-icons">update</i> Just Updated
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
 </div>
